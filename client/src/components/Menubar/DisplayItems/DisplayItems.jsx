2d9490a2a06b61aeeee2c8d69d3e1be82f47a699
EthanMcLa <essence102405@gmail.com>
2025-06-09 23:59:12 -0700

    Implemented Search functionality

--- client/src/components/Menubar/DisplayItems/DisplayItems.jsx
+++ client/src/components/Menubar/DisplayItems/DisplayItems.jsx
@@ -1,29 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './DisplayItems.css';
 import { AppContext } from '../../../context/AppContext';
 import Item from "../Item/Item.jsx";
 import SearchBox from '../SearchBox/SearchBox.jsx';
 
-const DisplayItems = () => {
+const DisplayItems = ({selectedCategory}) => {
       const {itemsData} = useContext(AppContext);
       const [searchText, setSearchText] = useState("");
       
       const filteredItems  = itemsData.filter(item =>  {
-        return item.name.toLowerCase().includes(searchText.toLowerCase());
+       if(!selectedCategory) return true;
+       return item.categoryId === selectedCategory;
          
-      })
+      }).filter(item => item.name.toLowerCase().includes(searchText.toLowerCase()));
 
       return (
     
       <div className="p-3">
             <div className="d-flex justify-content-between align-items-center align-items-center mb-4">
-                <div></div>
+                <div>
+                    
+                </div>
                 <div>
                     <SearchBox onSearch={setSearchText} />
                 </div>
             </div>
             <div className="row g-3">
-                </div> 
+                
             {filteredItems.map((item, index) => (
                 <div key={index} className="col-md-4 col-sm-6   ">
                     <Item 
@@ -31,10 +34,11 @@ const DisplayItems = () => {
                         itemPrice={item.price}
                         itemImage={item.imgUrl}
                         itemId={item.itemId} 
-                    />
-                </div>
+                    /></div> 
+                
             ))}
        </div>
+    </div>
     )
 }
 
